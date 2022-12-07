a46994c6f4f9b2710817c65f8ff844c578e15d40
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:12:44 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+
+    public static void main(String[] args) {
+        House house1 = new House("Via Verdi 5", 4);
+        House house2 = new House("Via Rosmini 23", 7);
+        House house3 = new House("Via dei fiori 7", 15);
+
+        System.out.println(house1.address + " è un condominio alto " + house1.numberOfFloors + " piani");
+        System.out.println(house2.address + " è un condominio alto " + house2.numberOfFloors + " piani");
+        System.out.println(house3.address + " è un condominio alto " + house3.numberOfFloors + " piani");
+    }
 }
